85be755c566cfe6e07f6cf7d2d6e9a339a95f231
VisCube <180091136+VisCube@users.noreply.github.com>
2025-03-22 15:24:12 +0500

    feat(openai): add image support for completions

--- src/openai/chat.py
+++ src/openai/chat.py
@@ -1,12 +1,19 @@
 from typing import Iterable
 
 import openai
-from discord import Message
-from openai.types.chat import ChatCompletion, ChatCompletionUserMessageParam
+from discord import Attachment, Message
+from openai.types.chat import (
+    ChatCompletion,
+    ChatCompletionContentPartImageParam as ImageContent,
+    ChatCompletionContentPartParam as Content,
+    ChatCompletionContentPartTextParam as TextContent,
+    ChatCompletionUserMessageParam as UserMessage
+)
+from openai.types.chat.chat_completion_content_part_image_param import ImageURL
 
 
-def complete(messages: Iterable[Message]) -> ChatCompletion:
-    gpt_messages = map(convert_message, messages)
+async def complete(messages: Iterable[Message]) -> ChatCompletion:
+    gpt_messages = [await convert_message(message) for message in messages]
 
     return openai.chat.completions.create(
         messages=gpt_messages,
@@ -14,9 +21,24 @@ def complete(messages: Iterable[Message]) -> ChatCompletion:
     )
 
 
-def convert_message(message: Message) -> ChatCompletionUserMessageParam:
-    return ChatCompletionUserMessageParam(
-        content=message.content,
+async def convert_message(message: Message) -> UserMessage:
+    text = TextContent(text=message.content, type="text")
+    media = [await convert_attachment(att) for att in message.attachments]
+
+    return UserMessage(
+        content=(text, *media),
         name=str(message.author.id),
         role="user"
     )
+
+
+async def convert_attachment(attachment: Attachment) -> Content:
+    match attachment.content_type.split("/")[0]:
+        case "image":
+            image_url = ImageURL(url=attachment.url, detail="auto")
+            return ImageContent(image_url=image_url, type="image_url")
+        case "text":
+            data = await attachment.read()
+            return TextContent(text=data.decode(), type="text")
+        case _:
+            return TextContent(text=attachment.filename, type="text")
