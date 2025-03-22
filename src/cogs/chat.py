f6dced1b731ff5a6ff6d1631541718ed87e3be22
VisCube <180091136+VisCube@users.noreply.github.com>
2025-03-22 16:13:33 +0500

    feat(pycord): add history support for completions

--- src/cogs/chat.py
+++ src/cogs/chat.py
@@ -1,4 +1,5 @@
 from discord import ApplicationContext, Bot, Cog, Message, message_command
+from discord.ext import commands
 
 from src.openai.chat import complete
 
@@ -15,6 +16,24 @@ class Chat(Cog):
         content = completion.choices[0].message.content
 
         await context.respond(content=content)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: Message):
+        if message.author.bot or not self.bot.user.mentioned_in(message):
+            return
+
+        history = message.channel.history()
+        thread, latest = [message], message
+        while latest.reference is not None:
+            latest = await history.get(id=latest.reference.message_id)
+            if latest is None:
+                break
+            thread.insert(0, latest)
+
+        async with message.channel.typing():
+            completion = await complete(messages=thread)
+            content = completion.choices[0].message.content
+            await message.reply(content=content)
 
 
 def setup(bot: Bot):
